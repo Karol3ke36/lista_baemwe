8ac11788a61bd9e11762da10af31e013c02405b0
Karol3ke36 <Karolpucyk9@gmail.com>
2025-12-12 08:38:21 +0100

    Koncowa wersja

--- app/src/main/java/com/example/listy_przepisy/specyfikacjaActivity.java
+++ app/src/main/java/com/example/listy_przepisy/specyfikacjaActivity.java
@@ -1,6 +1,9 @@
 package com.example.listy_przepisy;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class specyfikacjaActivity extends AppCompatActivity {
-
+    TextView textViewOpis, textViewMarka, textViewSpecyfikacja;
+    Button button;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,16 +25,27 @@ public class specyfikacjaActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         setContentView(R.layout.activity_specyfikacja);
         int index = getIntent().getIntExtra("INDEX", -1);
         String kategoria = getIntent().getStringExtra("KATEGORIA");
         Marka specyfikacja = null;
+
+        textViewMarka = findViewById(R.id.textViewMarka);
+        textViewOpis = findViewById(R.id.textViewOpis);
+        textViewSpecyfikacja = findViewById(R.id.textViewSpecyfikacja);
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+
         if(index>=0){
             specyfikacja = RepozytoriumMarek.zwrocAutoDanejKat(kategoria).get(index);
             wyswietlSpecyfikacje(specyfikacja);
         }
     }
     private void wyswietlSpecyfikacje(Marka specyfikacja){
-
+        textViewSpecyfikacja.setText("Specyfikacja:\n"+specyfikacja.getParametry());
+        textViewOpis.setText("Krótki opis:\n"+specyfikacja.getOpis());
+        textViewMarka.setText("Marka: "+specyfikacja.getNazwaMarki());
+        imageView.setImageResource(specyfikacja.getIdObrazka());
     }
 }
